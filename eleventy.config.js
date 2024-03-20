7adfaf9460b551164da58c3dc68e014ae100d8be
PsammystPrime <samuelprime384@gmail.com>
2024-03-20 13:20:41 +0300

    Configure date format with luxon

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,13 @@
+const { DateTime } = require("luxon");
+
+
+
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy('./src')
 
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+  return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+});
     return {
         dir: {
           input: "src",
