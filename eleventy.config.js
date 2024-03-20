8a6c059fb97c5dd111d2b7e6d3ec603d3f9f2b88
PsammystPrime <samuelprime384@gmail.com>
2024-03-20 09:45:48 +0300

    Create eleventy Config file

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig){
+  eleventyConfig.addPassthroughCopy('./src')
+
+    return {
+        dir: {
+          input: "src",
+          output: "public"
+        }
+      }
+}
